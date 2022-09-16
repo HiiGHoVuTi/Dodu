d485db17fb82d21d26d73715e85cd867b11fcdbf
Maxime <emixampons@gmail.com>
2022-09-16 10:55:27 +0200

    ✨: minimal parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,25 @@
 module Main where
 
+import Control.Applicative
+import Options.Applicative.Builder
+import Options.Applicative.Extra
+
+data Command = CommandHi
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = execParser opts >>= execCommand 
+  where
+    opts = info (optsParser <**> helper)
+         $ fullDesc
+        <> progDesc (unlines
+          [  
+          ])
+        <> header "Dodu, your friendly combinatory language toolkit"
+
+    optsParser =
+      subparser
+        ( command "say-hi" (info (pure CommandHi) (progDesc "Say hi to Dodu !"))
+        )
+
+execCommand :: Command -> IO ()
+execCommand CommandHi = putStrLn "Hi, Dodu !"
