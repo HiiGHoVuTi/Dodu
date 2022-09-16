23be9a1c6452a86b0d4ee07a673cd8e16736c31d
Yann <yann.didier54@gmail.com>
2022-09-16 17:49:59 +0200

    :construction: Added better literals for lambda
    added a LambdaVal type for literals

--- app/Lambda.hs
+++ app/Lambda.hs
@@ -1,10 +1,18 @@
 module Lambda (
-  LambdaExpr(..)
+  LambdaExpr(..),
+  LEnv,
 ) where
 
 import Data.Text
+import Data.Map
+
+data LambdaVal
+  = Rational
+  | List Rational
+
+data LEnv = Map Text LambdaExpr
 
 data LambdaExpr
   = LVar Text
   | LApp LambdaExpr LambdaExpr
-  | LLit Rational
+  | LLit LambdaVal
